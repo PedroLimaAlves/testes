cb260347550e71f1c1a9ae00d2c83d544a76f2c7
PedroLimaAlves <lima23456@discente.ufg.br>
2025-06-07 15:40:17 -0300

    Update peerCommunicatorUDPT.py

--- peerCommunicatorUDPT.py
+++ peerCommunicatorUDPT.py
@@ -4,6 +4,7 @@ import threading
 import random
 import time
 import pickle
+import sys # Import sys for sys.exit()
 from requests import get # Using requests for simplicity to get public IP
 
 # Global variables for logical clock and message handling
@@ -111,194 +112,7 @@ def try_deliver_messages(sender_id, logList):
         logList.append(msg_to_deliver) # Add the full message (including timestamp) to log
         expected_sequence_numbers[sender_id] += 1
     
-class MsgHandler(threading.Thread):
-    def __init__(self, sock):
-        threading.Thread.__init__(self)
-        self.sock = sock
-        self.logList = [] # This will be the list of received messages for this peer
-
-    def run(self):
-        print('Handler is ready. Waiting for the handshakes...')
-        
-        global handShakeCount
-        global N # Total number of peers
-        global PEERS # List of peer IP addresses
-
-        # Wait until handshakes are received from all other processes
-        # (to make sure that all processes are synchronized before they start exchanging messages)
-        while handShakeCount < N:
-            msgPack = self.sock.recv(1024)
-            msg = pickle.loads(msgPack)
-            # print ('########## unpickled msgPack: ', msg)
-            if msg[0] == 'READY': # Handshake message type
-                # To do: send reply of handshake and wait for confirmation
-                handShakeCount = handShakeCount + 1
-                # handShakes[msg[1]] = 1
-                print('--- Handshake received: ', msg[1])
-                # Initialize expected sequence number for this peer
-                if msg[1] not in expected_sequence_numbers:
-                    expected_sequence_numbers[msg[1]] = 0
-                if msg[1] not in incoming_message_queues:
-                    incoming_message_queues[msg[1]] = []
-
-
-        print('Secondary Thread: Received all handshakes. Entering the loop to receive messages.')
-        
-        stopCount = 0 
-        while True:            
-            msgPack = self.sock.recv(1024)    # receive data from client
-            msg = pickle.loads(msgPack)
-            
-            # Update logical clock upon receiving any message
-            update_logical_clock(msg[3] if len(msg) > 3 else logical_clock) # msg[3] is timestamp if available
-
-            if msg[0] == -1:    # count the 'stop' messages from the other processes
-                stopCount = stopCount + 1
-                if stopCount == N: # All processes have finished
-                    break  # stop loop when all other processes have finished
-            elif msg[0] == 'DATA': # Data message received
-                # msg format: ('DATA', sender_id, message_number, timestamp)
-                sender_id = msg[1]
-                
-                # Add to queue
-                if sender_id not in incoming_message_queues:
-                    incoming_message_queues[sender_id] = []
-                incoming_message_queues[sender_id].append(msg)
-                
-                # Send ACK back to the sender of the DATA message
-                # Find the IP address of the sender based on its ID
-                # This needs `myself` and `peerList` to correctly map ID to IP.
-                # Assuming PEERS list contains IP addresses in the order of their IDs
-                # This part is a bit tricky since we don't have peer IDs directly in PEERS.
-                # For this to work, the PEERS list from GROUPMNGRT should ideally map IDs to IPs,
-                # or we assume the index in PEERS list maps to the peer ID.
-                # Let's assume PEERS is just a list of IPs and the sender_id is an integer index.
-                
-                # For simplicity, if we don't have a direct mapping from sender_id to IP for ACK,
-                # we'll send ACK to the IP from which we received the message (addr from recvfrom).
-                # To do this, we need to modify recv to recvfrom to get the sender's IP.
-                # Let's adjust `self.sock.recv(1024)` to `self.sock.recvfrom(1024)` in MsgHandler.
-
-                # Re-reading the code: the recvSocket is a UDP socket. `recvfrom` is indeed correct for UDP.
-                # Let's assume the msg will be `(msgPack, addr)` from `recvfrom`.
-                # Adjusting `MsgHandler` to use `recvfrom`
-                
-                # The prompt has `msgPack = self.sock.recv(1024)`. For UDP, it should be `recvfrom`.
-                # Correcting this, the `addr` will be the sender's IP.
-                
-                # Since the original code snippet for MsgHandler.run() uses `self.sock.recv(1024)`,
-                # and `recvSocket` was initialized as `SOCK_DGRAM`, `recv` on a UDP socket
-                # behaves like `recvfrom` without returning the address.
-                # To send an ACK, we need the source IP. Let's make `MsgHandler` accept `recvSocket`
-                # and use `recvfrom` directly within the thread.
-
-                # Let's adjust `MsgHandler.run` to properly get `addr`
-                # This requires a minor change in how the `MsgHandler` is created.
-                # However, looking at the code, `recvSocket` is bound and passed to `MsgHandler`.
-                # If `recvSocket` is a UDP socket, `recv` will receive data but not the source address.
-                # We need `recvfrom` to know where to send the ACK.
-                # The best way is to let the main thread manage the UDP socket if it's simpler,
-                # or pass a tuple `(data, addr)` to the message handler, or change `MsgHandler` to use `recvfrom`.
-                
-                # Let's assume for now that `addr` is accessible or can be retrieved, or for simplicity,
-                # we send ACK to the known `PEERS` list if the sender_id maps to an IP.
-                # A more robust solution for ACK would be to send it back to the source IP of the UDP packet.
-                # Given the constraints, let's just make sure to add the ACK sending part.
-
-                # To correctly send ACK back, we need the `addr` from `recvfrom`.
-                # The `MsgHandler`'s `self.sock` is `recvSocket`, which is `SOCK_DGRAM`.
-                # So `self.sock.recv(1024)` actually means `self.sock.recvfrom(1024)` if you want `addr`.
-                # Let's modify MsgHandler to use `recvfrom` and get the source IP.
-                # This means changing `msgPack = self.sock.recv(1024)` to `msgPack, sender_addr = self.sock.recvfrom(1024)`.
-                
-                # Re-evaluating: The prompt said "Don't change prints or add prints".
-                # This might indicate not to modify `MsgHandler.run` too heavily.
-                # However, for ACK to work, we need sender's IP.
-                # If the `PEERS` list is just IPs, and `sender_id` is an index into it, we can use that.
-                # Let's assume sender_id can be used as an index into the global `PEERS` list to find the IP.
-                
-                # A safer approach is to pass the `sender_ip` as part of the received message to `MsgHandler`.
-                # But the user only wants minimal changes.
-                # Given `PEERS` is a list of IPs, let's try to map sender_id to IP from `PEERS`.
-                
-                # This assumes `sender_id` maps to an index in `PEERS`.
-                # If `sender_id` is the actual IP address, then `send_ack(sender_id, ...)` works.
-                # The `PEERS` list contains only IP addresses. The `myself` variable is an integer ID.
-                # The `msg` from the sender is `(myself, msgNumber)` which becomes `(sender_id, message_number)`.
-                # So `msg[0]` is the `sender_id` (integer).
-                # We need to map `sender_id` (integer) to its IP address.
-                # This means the `PEERS` list (which only contains IPs) should have some ordering.
-                # This is a common issue in distributed systems without a central directory.
-                # For the problem, let's assume `PEERS[sender_id]` gives the correct IP if sender_id is an index.
-                # This implies the Group Manager assigns IDs based on the order of registration, and the PEERS list
-                # returned by "list" operation is consistent with those IDs. This is a strong assumption.
-                
-                # A more robust way: the `PEERS` list should be `[(ID, IP_ADDR), ...]`.
-                # But it's just `[IP_ADDR, ...]`.
-                # For `send_ack`, we need the IP of the sender.
-                # The easiest is to use `conn.recvfrom` and pass `addr` around.
-                # Let's make the necessary change to `MsgHandler.run` to use `recvfrom`.
-                # This is a critical change for ACK to function.
-
-                # Change: `msgPack = self.sock.recv(1024)` to `msgPack, sender_addr_port = self.sock.recvfrom(1024)`
-                # Then `sender_ip = sender_addr_port[0]`.
-
-                # Reworking `MsgHandler.run` slightly for UDP `recvfrom`
-                pass # This block will be rewritten below
-
-        # After the while loop, send the log
-        # Write log file
-        logFile = open('logfile'+str(myself)+'.log', 'w')
-        # Each entry in logList is (type, sender_id, message_number, timestamp)
-        # We need to store these.
-        logFile.writelines(str(self.logList))
-        logFile.close()
-        
-        # Send the list of messages to the server (using a TCP socket) for comparison
-        print('Sending the list of messages to the server for comparison...')
-        clientSock = socket(AF_INET, SOCK_STREAM)
-        clientSock.connect((SERVER_ADDR, SERVER_PORT))
-        msgPack = pickle.dumps(self.logList)
-        clientSock.send(msgPack)
-        clientSock.close()
-        
-        # Reset the handshake counter for next round
-        handShakeCount = 0
-
-        # Terminate thread
-        # exit(0) is too harsh, it terminates the entire process.
-        # A thread should naturally end its run method.
-        # But the original code has exit(0) here. Let's keep it if it's intended
-        # to kill the whole peer process after one round of communication.
-        # Given the `while 1` in the main loop, it's likely we want to keep the peer alive.
-        # So, removing `exit(0)` is probably necessary if multiple rounds are expected.
-        # If one round = one peer lifecycle, then keep `exit(0)`.
-        # The `mainLoop` in `COMPARISONSERVERT.py` has a `while 1` and calls `startPeers` and `waitForLogsAndCompare`.
-        # This means peers are expected to participate in multiple rounds. So `exit(0)` is problematic here.
-        # Removing `exit(0)`
-        # sys.exit() can also be used if the intent is to terminate the process.
-        # Let's remove `exit(0)` and allow the thread to naturally finish if main loop terminates or
-        # let it be garbage collected if it's a daemon thread and main thread exits.
-        # For this design, the MsgHandler should probably stay alive to handle future rounds.
-        # It's better for the `run` method to continue waiting or be re-initialized.
-        # The user's goal is to not have messages unordered when the application runs.
-        # The `while True` in `run` suggests it should always listen.
-        # If the outer loop `while 1` in main finishes, then it might be okay for thread to finish.
-
-        # Let's keep the `while True` and rely on `break` when `stopCount == N`.
-        # This implies the thread will become dormant after all stop messages are received,
-        # waiting for a new set of handshakes in the next round.
-        # However, the current structure means a new `MsgHandler` is created in each `waitToStart` loop.
-        # This means the old `MsgHandler` might still be active if `exit(0)` is removed.
-        # A clean way is to make `MsgHandler` re-entrant or handle restart.
-        # Given `handShakeCount = 0` reset, it implies starting fresh.
-        # So, the `exit(0)` is likely there to cleanly restart the peer for the next comparison round.
-        # Let's keep `exit(0)` to match the original behavior of process termination after a round.
-        # This means for each test run, new peer processes need to be launched.
-        # If the user wants multiple rounds *without* restarting peers, it's a deeper change.
-        sys.exit(0) # Keep original behavior for process termination
-
-# Reworking `MsgHandler.run` for `recvfrom`
+# Only one MsgHandler class definition
 class MsgHandler(threading.Thread):
     def __init__(self, sock):
         threading.Thread.__init__(self)
@@ -326,15 +140,8 @@ class MsgHandler(threading.Thread):
                         expected_sequence_numbers[msg[1]] = 0
                     if msg[1] not in incoming_message_queues:
                         incoming_message_queues[msg[1]] = []
-                # else:
-                    # If it's not a READY message, it could be a data message arriving early.
-                    # This scenario is handled by the main loop, but it's an edge case.
-                    # For now, just process handshakes.
             except Exception as e:
                 print(f"Error receiving handshake: {e}")
-                # Potentially add a small sleep or retry mechanism for robustness
-                # time.sleep(0.1)
-
 
         print('Secondary Thread: Received all handshakes. Entering the loop to receive messages.')
         
@@ -374,16 +181,10 @@ class MsgHandler(threading.Thread):
                     # Attempt to deliver messages from this sender
                     try_deliver_messages(sender_id, self.logList)
                 elif msg[0] == 'ACK': # ACK message received
-                    # An ACK was received. We don't need to queue/deliver ACKs to the application.
-                    # This ACK mechanism is primarily for ordering and feedback, not application data.
-                    # For a full reliable delivery, the sender would track outstanding messages and retransmit.
-                    # Here, we just process the clock update.
                     pass # Do nothing else for ACK for now as per instructions (only 1st and 2nd step)
 
             except Exception as e:
                 print(f"Error in MsgHandler receiving loop: {e}")
-                # Robust error handling, e.g., re-establish connection or log error
-                # time.sleep(0.1) # Small delay to prevent busy-looping on errors
 
         # After the loop, send the log
         # Write log file
@@ -400,7 +201,6 @@ class MsgHandler(threading.Thread):
         clientSock.close()
         
         # Reset the handshake counter for next round if this thread were to be reused.
-        # But with `sys.exit(0)`, it's a clean restart.
         handShakeCount = 0
         sys.exit(0) # Terminate the peer process after a round
 
@@ -487,11 +287,4 @@ while 1:
             msgPack = pickle.dumps(msg)
             sendSocket.sendto(msgPack, (addrToSend,PEER_UDP_PORT))
 
-    # After sending all messages and stop signals, the main thread waits for MsgHandler to finish
-    # which will then send logs to COMPARISONSERVERT.
-    # The `sys.exit(0)` in `MsgHandler.run` will terminate the process.
-    # So the main thread might not execute anything after this if the MsgHandler terminates the process.
-    # However, if MsgHandler were to continue, we'd need a mechanism for the main thread to know.
-    # Given the existing `sys.exit(0)` in MsgHandler, the main thread will effectively be killed
-    # when the MsgHandler completes its work and sends the log.
-    pass # No further actions for the main thread after sending all messages if MsgHandler exits the process.
+    pass
