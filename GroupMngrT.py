d723408418acff205f25d0f148d64e0ba41507c7
PedroLimaAlves <lima23456@discente.ufg.br>
2025-06-07 18:36:16 -0300

    Update GroupMngrT.py

--- GroupMngrT.py
+++ GroupMngrT.py
@@ -6,32 +6,25 @@ port = GROUPMNGR_TCP_PORT
 membership = []
 
 def serverLoop():
-    serverSock = socket(AF_INET, SOCK_STREAM)
-    serverSock.bind(('0.0.0.0', port))
-    serverSock.listen(6)
-    print(f"Group Manager listening on port {port}")
-    while(1):
-        (conn, addr) = serverSock.accept()
-        msgPack = conn.recv(2048)
-        req = pickle.loads(msgPack)
-        if req["op"] == "register":
-            if (req["ipaddr"], req["port"]) not in membership:
-                membership.append((req["ipaddr"],req["port"]))
-                print ('Registered peer: ', req)
-            else:
-                print(f"Peer {req['ipaddr']}:{req['port']} already registered.")
-        elif req["op"] == "list":
-            # list = [] # This variable name masks the built-in list type. Let's rename it.
-            peer_list_to_send = []
-            for m in membership:
-                peer_list_to_send.append(m[0]) # Only send IP addresses
-            print ('List of peers sent to server: ', peer_list_to_send)
-            conn.send(pickle.dumps(peer_list_to_send))
-        else:
-            print(f"Unknown operation: {req['op']}")
-            # fix (send back an answer in case of unknown op)
-            conn.send(pickle.dumps({"status": "error", "message": "Unknown operation"}))
+  serverSock = socket(AF_INET, SOCK_STREAM)
+  serverSock.bind(('0.0.0.0', port))
+  serverSock.listen(6)
+  while(1):
+    (conn, addr) = serverSock.accept()
+    msgPack = conn.recv(2048)
+    req = pickle.loads(msgPack)
+    if req["op"] == "register":
+      membership.append((req["ipaddr"],req["port"]))
+      print ('Registered peer: ', req)
+    elif req["op"] == "list":
+      list = []
+      for m in membership:
+        list.append(m[0])
+      print ('List of peers sent to server: ', list)
+      conn.send(pickle.dumps(list))
+    else:
+      pass # fix (send back an answer in case of unknown op
 
-        conn.close()
+  conn.close()
 
 serverLoop()
