f2f2ac9bd4d03c50bf09704c4a7ed233216016e8
PedroLimaAlves <lima23456@discente.ufg.br>
2025-06-07 18:01:02 -0300

    Update constMPT.py

--- constMPT.py
+++ constMPT.py
@@ -1,11 +1,10 @@
 PEER_UDP_PORT = 6789
 PEER_TCP_PORT = 5679
-N = 4 # número de pares
-SERVER_ADDR ='3.82.138.43'
+N_VIRGINIA = 2  # Número de pares na região da Virgínia
+N_OREGON = 2    # Número de pares na região de Oregon
+N = N_VIRGINIA + N_OREGON # Número total de pares
+
+SERVER_ADDR ='3.82.138.43' # IP do ComparisonServer na Virginia
 SERVER_PORT = 5678
-GROUPMNGR_ADDR = '98.81.198.30'
+GROUPMNGR_ADDR = '98.81.198.30' # IP do GroupManager na Virginia (Atualizado para o IP que você forneceu)
 GROUPMNGR_TCP_PORT = 5680
-
-
-ACK_TIMEOUT = 1
-MAX_RETRIES = 20
