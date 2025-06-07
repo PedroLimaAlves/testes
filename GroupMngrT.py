7369434ff4971b7f2e6bf3875fe34dc15796ac9b
PedroLimaAlves <lima23456@discente.ufg.br>
2025-06-07 17:42:29 -0300

    Update GroupMngrT.py

--- GroupMngrT.py
+++ GroupMngrT.py
@@ -6,25 +6,32 @@ port = GROUPMNGR_TCP_PORT
 membership = []
 
 def serverLoop():
-  serverSock = socket(AF_INET, SOCK_STREAM)
-  serverSock.bind(('0.0.0.0', port))
-  serverSock.listen(6)
-  while(1):
-    (conn, addr) = serverSock.accept()
-    msgPack = conn.recv(2048)
-    req = pickle.loads(msgPack)
-    if req["op"] == "register":
-      membership.append((req["ipaddr"],req["port"]))
-      print ('Registered peer: ', req)
-    elif req["op"] == "list":
-      list = []
-      for m in membership:
-        list.append(m[0])
-      print ('List of peers sent to server: ', list)
-      conn.send(pickle.dumps(list))
-    else:
-      pass # fix (send back an answer in case of unknown op
+    serverSock = socket(AF_INET, SOCK_STREAM)
+    serverSock.bind(('0.0.0.0', port))
+    serverSock.listen(6)
+    print(f"Group Manager listening on port {port}")
+    
+    while True:
+        conn, addr = serverSock.accept()
+        msgPack = conn.recv(2048)
+        req = pickle.loads(msgPack)
 
-  conn.close()
+        if req["op"] == "register":
+            peer_entry = (req["ipaddr"], req["port"])
+            if peer_entry not in membership:
+                membership.append(peer_entry)
+            print('Registered peer: ', req)
+            conn.send(pickle.dumps("ACK: Registered"))
+            
+        elif req["op"] == "list":
+            peer_list = [m[0] for m in membership]
+            print('List of peers sent to server: ', peer_list)
+            conn.send(pickle.dumps(peer_list))
+            
+        else:
+            print(f"Unknown operation requested: {req.get('op', 'N/A')}")
+            conn.send(pickle.dumps("ERROR: Unknown operation"))
+        
+        conn.close()
 
 serverLoop()
