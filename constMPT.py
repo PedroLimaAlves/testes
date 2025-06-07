b9f703d0a50d59a72372c1216bad3d07a57fc289
PedroLimaAlves <lima23456@discente.ufg.br>
2025-06-07 16:27:09 -0300

    Update constMPT.py

--- constMPT.py
+++ constMPT.py
@@ -1,7 +1,10 @@
 PEER_UDP_PORT = 6789
 PEER_TCP_PORT = 5679
-N = 2
-SERVER_ADDR ='3.82.138.43'
+N = 4 # número de pares
+SERVER_ADDR ='3.95.155.5'
 SERVER_PORT = 5678
-GROUPMNGR_ADDR = '98.81.198.30'
+GROUPMNGR_ADDR = '44.204.28.248'
 GROUPMNGR_TCP_PORT = 5680
+
+ACK_TIMEOUT = 0.1 # Tempo em segundos para esperar por um ACK antes de retransmitir
+MAX_RETRIES = 5 # Número máximo de retransmissões para uma mensagem
